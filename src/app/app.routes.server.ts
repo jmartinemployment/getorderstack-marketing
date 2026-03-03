53557d2c6b05de53062b868f7e0e4a85c541b8b7
Jeff Martin <jmartinpersonal@yahoo.com>
2026-03-03 08:36:54 -0500

    fix(ssr): enumerate prerender routes to prevent wildcard redirect

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'features',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'pricing',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'demo',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'integrations',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'get-started',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
